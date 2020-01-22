f3c221f3f59b27ae6e38fce90b05e029adc6697b
Adebola Omoboya <adeomoboya@gmail.com>
2020-01-22 11:02:26 +0100

    The First Cut

---
+++ src/main/java/io/factorialsystems/faspetclinic/FasPetClinicApplication.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.faspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/factorialsystems/faspetclinic/FasPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.faspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
