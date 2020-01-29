aaeb8045c881360cc9fc7ddcb132b81ccc6bfe55
Adebola Omoboya <adeomoboya@gmail.com>
2020-01-29 19:05:25 +0100

    Added BaseEntity class closes #9

--- pet-clinic-data/src/main/java/io/factorialsystems/faspetclinic/data/model/Person.java
+++ pet-clinic-data/src/main/java/io/factorialsystems/faspetclinic/data/model/Person.java
@@ -1,6 +1,6 @@
 package io.factorialsystems.faspetclinic.data.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/io/factorialsystems/faspetclinic/data/model/PetType.java
+++ pet-clinic-data/src/main/java/io/factorialsystems/faspetclinic/data/model/PetType.java
@@ -1,6 +1,6 @@
 package io.factorialsystems.faspetclinic.data.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
