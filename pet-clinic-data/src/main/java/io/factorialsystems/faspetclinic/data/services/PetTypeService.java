e8777682368bab70c129fec25d18d8fec9997fa2
Adebola Omoboya <adeomoboya@gmail.com>
2020-02-19 17:08:51 +0100

    Create PetType Map Service closes #19

---
+++ pet-clinic-data/src/main/java/io/factorialsystems/faspetclinic/data/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package io.factorialsystems.faspetclinic.data.services;
+
+import io.factorialsystems.faspetclinic.data.model.PetType;
+
+public interface PetTypeService extends BaseService<PetType, Long> {
+}
